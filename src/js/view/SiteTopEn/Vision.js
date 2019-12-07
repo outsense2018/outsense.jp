cb25482187ffbfdc4baf5c8bc6a81c357f7c7379
a-ito2021 <a-ito@outsense.jp>
2019-12-07 17:21:16 +0900

    vision部分を編集

--- src/js/view/SiteTopEn/Vision.js
+++ src/js/view/SiteTopEn/Vision.js
@@ -5,15 +5,15 @@ export default () => state => (
     <h1>Vision</h1>
     <h2>
       We will make you live in anywhere,<br />
-      even if,there is in SPACE.
+      even if, there is in SPACE.
     </h2>
     <p>
-      Our name, OUTSENSE, comes from "We getting OUT from boring common SENSE".
+      Our name, OUTSENSE, comes from "We getting OUT from boring common SENSE".<br />
       We think space has ultimate power to change our perspective dramatically,
       and want to make people feel more bright.
     </p>
     <p>
-    To live outside of the earth, human being will meet unexpected world,<br />
+    Living outside of the earth, then human being will meet unexpected world,<br />
     experience, feeling, culture, ability... completely new thing.
     </p>
     <p>
