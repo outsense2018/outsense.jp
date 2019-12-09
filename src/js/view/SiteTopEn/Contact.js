d13561360b69c8ccb2e05be262c4bc3489fc23fc
a-ito2021 <a-ito@outsense.jp>
2019-12-09 16:45:50 +0900

    諸々の修正点を反映できるとこはした

--- src/js/view/SiteTopEn/Contact.js
+++ src/js/view/SiteTopEn/Contact.js
@@ -1,8 +1,9 @@
 import { h } from "hyperapp"; // @jsx h
 
 export default () => state => (
-  <section>
+  <section className="contactEn">
     <h1>Contact</h1>
+    <h3>Please contact via email</h3>
     <a href="mailto:info@outsense.jp">
       <h4>株式会社OUTSENSE / OUTSENSE, inc.</h4>
       info@outsense.jp
